cbdcac38e9654c6e2c05a9f7be753a62a21cf53f
Mark Fasel <mark.fasel@daveramsey.com>
2018-12-03 14:27:55 -0600

    End Video 46

---
+++ 13 - JavaScript Modules and Using npm/es6modules/app.js
@@ -0,0 +1,8 @@
+import { uniq } from 'lodash';
+import insane from 'insane';
+import jsonp from 'jsonp';
+import { apiKey, url, sayHi } from './src/config';
+
+sayHi('Mark')
+const ages = [1, 1, 3, 4, 5, 6, 6, 12, 23];
+
---
+++ 13 - JavaScript Modules and Using npm/es6modules/src/config.js
@@ -0,0 +1,9 @@
+export const apiKey = 'abc123'; // named export - have to use apiKey
+
+export const url = 'https://markfasel.com';
+
+export function sayHi(name) {
+	console.log(`Hello there ${name}`);
+}
+
+//export default apiKey;
